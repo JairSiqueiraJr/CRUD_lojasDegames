63d44098a5a801ae444a2f86449652835a04af4e
Jairlive <juniorwin67@gmail.com>
2022-07-11 19:34:32 -0300

    Fist commit do projeto da loja de games

---
+++ src/main/java/com/lojadegames/ProjetoLojadegamesApplication.java
@@ -0,0 +1,13 @@
+package com.lojadegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojadegamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLojadegamesApplication.class, args);
+	}
+
+}
